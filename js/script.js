1189af0f1d6a6a6b7c823644cc54b3f867e21084
Damjan15 <stamenovdamjan5@gmail.com>
2025-07-10 00:19:04 +0200

    feat: hamburger toggle functionality created

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 // ====== TOGGLE HAMBURGER MENU ======
 document.addEventListener("DOMContentLoaded", function () {
-  const toggleButton = document.getElementById("toggle-button");
+  const openMenuBtn = document.getElementById("open-menu-btn");
+  const closeMenuBtn = document.getElementById("close-menu-btn");
   const mobileMenu = document.getElementById("mobile-menu");
 
-  toggleButton.addEventListener("click", function () {
-    console.log("Clicked");
+  openMenuBtn.addEventListener("click", function () {
+    mobileMenu.classList.remove("translate-x-full");
+    mobileMenu.classList.add("translate-x-0");
+  });
+
+  closeMenuBtn.addEventListener("click", function () {
+    mobileMenu.classList.add("translate-x-full");
   });
 });
