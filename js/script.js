93e8916d94016e6c6b392544d62893e03a2cda46
Damjan15 <stamenovdamjan5@gmail.com>
2025-07-09 13:56:12 +0200

    feat: header & hero section created

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// ====== TOGGLE HAMBURGER MENU ======
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("toggle-button");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  toggleButton.addEventListener("click", function () {
+    console.log("Clicked");
+  });
+});
